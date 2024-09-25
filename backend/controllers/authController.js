0b9fee01f169560d1199593bf4f02cbad8ee02eb
Sarah Gad <sarahgad298@gmail.com>
2024-09-25 07:57:23 +0300

    created the login controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,8 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
-const { User, validateRegisterUser } = require('../models/User');
+const { User, validateRegisterUser, validateLoginUser } = require('../models/User');
 
+// register User
 module.exports.registerUserCtrl = asyncHandler(async (req, res) => {
   const { error } = validateRegisterUser(req.body);
   if (error) {
@@ -22,5 +23,36 @@ module.exports.registerUserCtrl = asyncHandler(async (req, res) => {
   });
   await user.save();
 
+  // later i will verify account here
+
   res.status(201).json({ message: 'You registered successfully, please log in' });
+});
+
+// Login User
+module.exports.loginUserCtrl = asyncHandler(async (req, res) => {
+  const { error } = validateLoginUser(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).json({ message: 'Invalid email or password' });
+  }
+
+  const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+  if (!isPasswordMatch) {
+    return res.status(400).json({ message: 'Invalid email or password' });
+  }
+
+  // Later I will verify account here as well
+
+  const token = user.generateAuthToken();
+
+  res.status(200).json({
+    _id: user._id,
+    isAdmin: user.isAdmin,
+    profilePhoto: user.profilePhoto,
+    token,
+  });
 });
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { registerUserCtrl } = require('../controllers/authController');
+const { registerUserCtrl, loginUserCtrl } = require('../controllers/authController');
 
 router.post('/register', registerUserCtrl);
+router.post('/login', loginUserCtrl);
 module.exports = router;
