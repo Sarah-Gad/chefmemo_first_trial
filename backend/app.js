c12f55c2909ef31a617a2a5481075104989d1354
Sarah Gad <sarahgad298@gmail.com>
2024-09-25 14:12:35 +0300

    added getAllUsersCtrl controller for admins only

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ connectToDb();
 const app = express();
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoute'));
+app.use('/api/users', require('./routes/usersRoute'));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
---
+++ backend/routes/usersRoute.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { getAllUsersCtrl } = require('../controllers/usersController');
+const { verifyTokenAndAdmin } = require('../middlewares/verifyToken');
+
+router.route('/profile').get(verifyTokenAndAdmin, getAllUsersCtrl);
+module.exports = router;
