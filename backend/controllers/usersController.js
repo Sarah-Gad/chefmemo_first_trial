5fae49e2416e68d2ecdf7ebd6fc83179da67aaf3
Sarah Gad <sarahgad298@gmail.com>
2024-09-25 15:03:04 +0300

    created the getUserProfileCtrl controller to get the user profile

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -2,6 +2,14 @@ const asyncHandler = require('express-async-handler');
 const { User } = require('../models/User');
 
 module.exports.getAllUsersCtrl = asyncHandler(async (req, res) => {
-  const users = await User.find();
+  const users = await User.find().select('-password');
   res.status(200).json(users);
+});
+
+module.exports.getUserProfileCtrl = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+  res.status(200).json(user);
 });
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { getAllUsersCtrl } = require('../controllers/usersController');
+const { getAllUsersCtrl, getUserProfileCtrl } = require('../controllers/usersController');
 const { verifyTokenAndAdmin } = require('../middlewares/verifyToken');
+const validateObjectId = require('../middlewares/validateObjectId');
 
 router.route('/profile').get(verifyTokenAndAdmin, getAllUsersCtrl);
+router.route('/profile/:id').get(validateObjectId, getUserProfileCtrl);
 module.exports = router;
