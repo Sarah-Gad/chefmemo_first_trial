a2165ff6edf2bf717bcf41bf08265d57fc8f3cc6
Sarah Gad <sarahgad298@gmail.com>
2024-10-02 05:09:08 +0300

    adjusted the color palette of the web app

--- frontend/src/components/header/HeaderLeft.jsx
+++ frontend/src/components/header/HeaderLeft.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom"
 import { ChefHat, X, Menu } from 'lucide-react';
 
-const HeaderLeft = ({ toggle, setToggle}) => {
+const HeaderLeft = ({ toggle, setToggle }) => {
     return (
-        <div className="header-left">
-            <Link to="/" className="header-logo">
-                <strong>Chef Memo</strong>
-                <ChefHat size={26} />
-            </Link>
-            <div onClick={() => setToggle(prev => !prev)} className="header-menu">
-            {toggle ? <X size={35} className="menu-icon" /> : <Menu size={35} className="menu-icon" />}
-            </div>
+      <div className="header-left">
+        <Link to="/" className="header-logo">
+          <strong>Chef Memo</strong>
+          <ChefHat className="chefhatlogo" size={26} />
+        </Link>
+        <div onClick={() => setToggle(prev => !prev)} className="header-menu">
+          {toggle ? <X size={35} className="menu-icon" /> : <Menu size={35} className="menu-icon" />}
         </div>
+      </div>
     );
-}
+  };
 
 export default HeaderLeft;
