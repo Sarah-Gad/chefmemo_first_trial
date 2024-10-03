df373f52933ddb08626b273f4b0f7b47ecfbec9c
Sarah Gad <sarahgad298@gmail.com>
2024-10-03 07:37:58 +0300

    designed the recipes page using the components RcipeList, Sidebar and Pagination

--- frontend/src/pages/recipes-page/RecipesPage.jsx
+++ frontend/src/pages/recipes-page/RecipesPage.jsx
@@ -1,5 +1,20 @@
+import "./recipes-page.css";
+import { categories, recipes } from "../../dummyData";
+import RcipeList from "../../components/recipes/RecipeList";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Pagination from "../../components/pagination/Pagination";
+
 const RecipesPage = () => {
-    return ( <h1>Recipes Page</h1> );
-}
+
+  return (
+    <>
+      <section className="recipes-page">
+        <RcipeList recipes={recipes} />
+        <Sidebar categories={categories} />
+      </section>
+      <Pagination />
+    </>
+  );
+};
 
 export default RecipesPage;
