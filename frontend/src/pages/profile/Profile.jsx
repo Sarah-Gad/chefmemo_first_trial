9a1520c941cdc16c0b2c1f190e9b8710d7141e1c
Sarah Gad <sarahgad298@gmail.com>
2024-10-10 03:13:20 +0300

    created the reducer "setProfile" to manage the profile state

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -2,19 +2,26 @@ import "./profile.css";
 import RecipeList from "../../components/recipes/RecipeList";
 import { recipes } from "../../dummyData";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import UpdateProfileModal from "./UpdateProfileModal";
 import swal from "sweetalert";
 import { toast } from "react-toastify";
+import { getUserProfile } from "../../redux/apiCalls/profileApiCall";
 
 
 const Profile = () => {
+  const dispatch = useDispatch();
+  const { profile } = useSelector(state => state.profile);
 
   const [updateProfile, setUpdateProfile] = useState(false);
   const [file, setFile] = useState(null);
 
+  const { id } = useParams();
   useEffect(() => {
+    dispatch(getUserProfile(id))
     window.scrollTo(0, 0);
-  }, []);
+  }, [id]);
 
   // Form Submit Handler
   const formSubmitHandler = (e) => {
@@ -47,7 +54,7 @@ const Profile = () => {
     <section className="profile">
       <div className="profile-header">
         <div className="profile-image-wrapper">
-          <img src={file ? URL.createObjectURL(file) : "/images/user-avatar.png"} alt="" className="profile-image" />
+          <img src={file ? URL.createObjectURL(file) : profile?.profilePhoto.url } alt="" className="profile-image" />
           <form onSubmit={formSubmitHandler}>
           <abbr title="choose profile photo">
             <label
@@ -65,13 +72,15 @@ const Profile = () => {
             <button type="submit" className="upload-profile-photo-btn">upload</button>
           </form>
         </div>
-        <h1 className="profile-username">Sarah Gad</h1>
+        <h1 className="profile-username">{profile?.username}</h1>
         <p className="profile-bio">
-          Hello, I'm Sarah. I am a web developer
+          {profile?.bio}
         </p>
         <div className="user-date-joined">
           <strong>Member Since: </strong>
-          <span>Fri jan 09 2024</span>
+          <span>
+            {new Date(profile?.createdAt).toDateString()}
+          </span>
         </div>
         <button onClick={() => setUpdateProfile(true)} className="profile-update-btn">
           <i className="bi bi-file-person-fill"></i>
@@ -79,7 +88,7 @@ const Profile = () => {
         </button>
       </div>
       <div className="profile-recipes-list">
-        <h2 className="profile-recipes-list-title">Recipes of sarah</h2>
+        <h2 className="profile-recipes-list-title">Recipes of {profile?.username} </h2>
         <RecipeList recipes={recipes} />
       </div>
       <button onClick={deleteAccountHandler} className="delete-account-btn">
